2c6211d7918a7e309fbaf98b1020e10c0b5028b2
sovonthy.leng <sovonthy.leng@PNC.LAN>
2019-12-16 09:23:19 +0700

    add button member

--- js/script.js
+++ js/script.js
@@ -9,10 +9,42 @@ $(document).ready(() => {
     $('#recipe').on('change', function () {
         var recipes = $('#recipe').val();
         getRecipe(recipes);
-
-
+      
     });
+    $('#plus').on('change', function () {
+        var add = $('$input').val();
+        if(num < 15) {
+            addMembers(num);
+           }
+    })
 });
+function  getMember(nubGuest){
+    var choose = "";
+        choose +=`
+        <h4>Number of person</h4>  &nbsp; &nbsp;
+            <div class="input-group-append">
+                <button class="btn btn-outline-secondary" id="minus" type="submit">-</button>
+            </div>
+            <input type="text" id="input" class="form-control text-center" disabled value="${nubGuest}">
+            <div class="input-group-append">
+                <button class="btn btn-outline-secondary" id="plus" type="submit">+</button>
+            </div>
+        `;
+        $('#choose').html(choose);
+
+ // function click on icon sum
+$("#plus").on('click', function () {
+    var number = parseInt($("#input").val());
+    sum(number);
+})
+// function click on icon minus
+$("#minus").on('click', function () {
+    var number = parseInt($("#input").val());
+    minus(number);
+})
+}
+
+
 function requestApi() {
     $.ajax({
         dataType: 'json',
@@ -34,46 +66,46 @@ function chooseRecipe(rechipe) {
 }
 function getRecipe(rechipeId) {
     alldata.forEach(element => {
-        if (element.id == rechipeId) {
-            getEachRecipe(element.name, element.iconUrl);
-            eachIngredient(element.ingredients);
-            eachStep(element.instructions);
+        var{id, name,iconUrl, ingredients, instructions,nbGuests} = element;
+        if (id == rechipeId) {
+            getEachRecipe(name, iconUrl);
+            eachIngredient(ingredients);
+            eachStep(instructions);
+            getMember(nbGuests);
+            sum(element);
+            minus(element);
+            getQuanlities = element;
+            //get OldGuest
+            oldGuest = element.nbGuests;
         }
-
-
     });
 }
 
-
 var getEachRecipe = (name, img) => {
-    var result = "";
-    result += `
+    var nameOfRecipe = "";
+    nameOfRecipe += `
         <h3>${name}</h3> 
     `;
-    $('#card').html(result);
+    $('#card').html(nameOfRecipe);
 
-    var results = "";
-    results += `
-        <img src="${img}" width="120"> 
+    var Recipe = "";
+    Recipe += `
+        <img src="${img}" width="200"> 
     `;
-    $('#cards').html(results);
+    $('#cards').html(Recipe);
 
 }
-
-
 /// Get ingredients
 $('#ingredient').hide();
 function eachIngredient(ingredient) {
     var ing = "";
     ingredient.forEach(el => {
         ing += `
-    
             <tr>
-              <td><img src="${el.iconUrl}" width="50"></td>
-                <td>${el.name}</td>
+              <td><img src="${el.iconUrl}" width="100"></td>
                 <td>${el.quantity}</td>
                 <td>${el.unit[0]}</td>
-                
+                <td>${el.name}</td>
             </tr>
         `;
     });
@@ -81,14 +113,13 @@ function eachIngredient(ingredient) {
     $('#ingredient').show();
 }
 
-
 //// get instruction
 $('#introduction').hide();
 function eachStep(step) {
     var steps = step.split('<step>');
-    var listStep = "";
+    var cutStep = "";
     for(var i =1; i < steps.length; i++) {
-        listStep += `
+        cutStep += `
            <li class = "list-group-item" style= "border:none;">
             <strong class = "text-primary">Step:${i}</strong>
             <br>
@@ -97,13 +128,47 @@ function eachStep(step) {
             </li>
         `
     }
-    $('#instruction').html(listStep);
+    $('#instruction').html(cutStep);
     $('#introduction').show();
 }
 
 
 
+function sum(number) {
+    var add = parseInt(number) +1;
+    if(add <= 15) {
+        $("#input").val(add);
+        getPerson($("#input").val());
+    }
+}
+// decrease value when click on icon minus
+function minus(number) {
+    var minus = parseInt(number)-1;
+    if(minus >= 1) {
+        $("#input").val(minus);
+        getPerson($("#input").val());
+    }
+}
 
 
-
-
+//new quantity = new guest * old quantity / old quest
+// function for new quanlity
+function getPerson(person) {
+    var quantities;
+    var newQuanlity;
+    var result = "";
+    getQuanlities.ingredients.forEach(element => {
+        var {quantity,iconUrl,name,unit} = element;
+        quantities = quantity/oldGuest;
+        newQuanlity = quantities*person;
+        result += `
+        <tr>
+        <td><img src="${iconUrl}" style="width:50px"></td>
+        <td id='quantity'>${newQuanlity}</td>
+        <td>${unit[0]}</td>
+        <td>${name}</td>
+        </tr>
+    `;
+    });
+     $("#ingredient").html(result);
+}
